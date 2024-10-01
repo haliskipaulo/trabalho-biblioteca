da94feceed86f1e0c119635a1c1eaa17f82a992a
Nathan Santos <nathan_lucindosantos@hotmail.com>
2024-10-01 15:14:54 -0300

    Adicionando Livro e Criação de Pastas

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import sqlite3
+import os
+
+
+# === CRIAR ESTRUTURA DE PASTAS ===
+def criar_pastas():
+    os.makedirs('TrabalhoPythonLivraria/data', exist_ok=True)
+    os.makedirs('TrabalhoPythonLivraria/backups', exist_ok=True)
+    os.makedirs('TrabalhoPythonLivraria/exports', exist_ok=True)
 
 # === BD AQUI EM BAIXO =====
 
@@ -11,15 +19,23 @@ def create_db():
             titulo TEXT NOT NULL,
             autor TEXT NOT NULL,
             ano_publicacao INTEGER,
-            preco REAL
+            preco FLOAT
         )
     ''')
     conn.commit()
     conn.close()
 
 
+
 # === ADICIONAR UM NOVO LIVRO AQUI EM BAIXO =====
 
+def adicionar_livro(titulo, autor, ano_publicacao, preco):
+    conn = sqlite3.connect('TrabalhoPythonLivraria/data/livraria.db')
+    consulta = conn.cursor()
+    consulta.execute('''INSERT INTO livros (titulo, autor, ano_publicacao, preco) VALUES (?, ?, ?, ?)''', (titulo, autor,ano_publicacao, preco))
+    conn.commit()
+    conn.close()    
+    # Usado ? no lugar de $s e tals, pois o proprio sql substitui os valores. E é mais seguro, mesmo nesse caso n precisando
 
 
 # === EXIBIR TODOS OS LIVROS AQUI EM BAIXO =====
@@ -51,6 +67,11 @@ def create_db():
 
 
 
+# === MAIN =====
+
+criar_pastas()  
+create_db()  
+conn = sqlite3.connect('TrabalhoPythonLivraria/data/livraria.db')
 
 
 while True:
@@ -69,7 +90,12 @@ while True:
         
     opcao = input('Escolha uma opção: ')
         
-    # if opcao == '1':
+    if opcao == '1':
+        titulo = input('Digite o título do livro: ')
+        autor = input('Digite o nome do autor do livro: ')
+        ano_publicacao = int(input('Digite o ano de publicação: '))
+        preco = float(input('Digite o preço do livro: '))
+        adicionar_livro(titulo, autor, ano_publicacao, preco)    
            
     # elif opcao == '2':
             
@@ -85,8 +111,11 @@ while True:
             
     # elif opcao == '8':
             
-    # elif opcao == '9':
-            
-    #     break 
-    # else:
-    #     print('Opção inválida.')
+    elif opcao == '9':           
+        print('==== Saindo... ====')
+        print('==== Volte Sempre ====')
+        print()
+        break
+
+    else:
+        print('Opção inválida.')
